cffe436faaecce4bcb4eeb6a3e9583eb5cb8bad5
Pragmatic Software <pragma78@gmail.com>
2020-07-12 23:41:39 -0700

    Add test/test.pl test script

---
+++ test/test.pl
@@ -0,0 +1,26 @@
+/* This is a Plang test script
+ *
+ * You can run it via ./plang < test/test.pl
+ */
+
+// this is a familiar C/C++ style comment to the EOL
+
+# EOL comments can also start with a pound
+
+# one-statement functions do not need braces
+fn square(x)    x * x
+
+println(square(4))
+
+# $"" and $'' strings evaluate statements inside {}s
+var foo = 4
+println($"square of {foo}: {square(foo)}")  # prints "square of 4: 16"
+
+# commas and semi-colons are largely optional
+fn test
+  (x y)
+{
+  var a = x
+  a + y
+}
+test(2 3)  # prints 5
