8ff4a975885bfe44930bcb0912fa8ebd2be1134b
Pragmatic Software <pragma78@gmail.com>
2020-07-20 03:04:38 -0700

    examples/fibonacci.pl: Plang now has while loops!

--- examples/fibonacci.pl
+++ examples/fibonacci.pl
@@ -1,19 +1,4 @@
 fn fib(n) n == 1 ? 1 : n == 2 ? 1 : fib(n-1) + fib(n-2)
 
-# Plang doesn't have loops yet.
-
-print(fib(1))
-print(fib(2))
-print(fib(3))
-print(fib(4))
-print(fib(5))
-print(fib(6))
-print(fib(7))
-print(fib(8))
-print(fib(9))
-print(fib(10))
-print(fib(11))
-print(fib(12))
-print(fib(13))
-print(fib(14))
-print(fib(15))
+var i = 0;
+while (++i < 16) print(fib(i))
